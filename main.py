b5402bd8f7dceb5537d48df73615c3824529286d
coconoca <coconoca.8039@gmail.com>
2022-08-07 16:39:54 +0900

    deco

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #デコレータの勉強
+#始めに実行されるのは、func = func1(func2)
+#def func1()の引数にfunc2が設定される
+#def wrapperは実行されず、しかしエラーも出さずにreturn wrapperをfunc1に返す
+#func = wrapper　となる
+#func1()はwrapper()と同義
+#wrapper()が実行されることによりdef wrapper()が実行されることになる
+
 def func1(f):
     def wrapper():
         print('開始')
