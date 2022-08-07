60d640839a4d25affed3a64ed57704e3917ecbd8
coconoca <coconoca.8039@gmail.com>
2022-08-07 17:08:15 +0900

    deco

--- main.py
+++ main.py
@@ -6,15 +6,28 @@
 #func1()はwrapper()と同義
 #wrapper()が実行されることによりdef wrapper()が実行されることになる
 
-def func1(f):
-    def wrapper():
-        print('開始')
-        f()
-        print('終了')
+import datetime
+
+def print_datetime(f):
+    def wrapper(*args,**kwargs):    #可変長引数
+        print(f'開始:{datetime.datetime.now()}')
+        f(*args,**kwargs)
+        print(f'終了:{datetime.datetime.now()}')
+        print('')
     return wrapper
 
-def func2():
-    print("Called func2")
+#@~~でデコレーション
+@print_datetime
+def calc1(base,height):
+    print('calc1の実行')
+    print(base * height / 2)
 
-func = func1(func2)
-func()
+calc1(3,15)
+#print_datetime(calc)(3,10)
+
+@print_datetime
+def calc2(a,b,c,d):
+    print('calc2の実行')
+    print(a*b*c*d)
+
+calc2(2,2,2,2)
