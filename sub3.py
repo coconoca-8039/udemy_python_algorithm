2f16bb19bfb4b59d4e623ac231a13583b1a40a68
coconoca <coconoca.8039@gmail.com>
2022-08-08 21:05:37 +0900

    Kapsel

--- sub3.py
+++ sub3.py
@@ -1,11 +1,14 @@
 #https://www.geeksforgeeks.org/polybius-square-cipher/
 # Python Program to implement polybius cipher
 # function to display polybius cipher text
+#https://ja.wikipedia.org/wiki/ポリュビオスの暗号表
+
 def polybiusCipher(s):
     # convert each character to its encrypted code
     for char in s:
 
         # finding row of the table
+        #ordは文字をunicord値に変換する関数
         row = int((ord(char) - ord('a')) / 5) + 1
 
         # finding column of the table
@@ -21,15 +24,13 @@ def polybiusCipher(s):
             if col == 1:
                 col = 6
                 row = row - 1
-
             col = col - 1
 
         print(row, col, end='', sep='')
 
-
 # Driver's Code
 if __name__ == "__main__":
-    s = "g"
+    s = "yuito"
 
     # print the cipher of "geeksforgeeks"
     polybiusCipher(s)
