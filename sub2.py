14343ba755703f8fac812aae58b8d2a4f1938060
coconoca <coconoca.8039@gmail.com>
2022-08-08 09:45:42 +0900

    Kapsel

--- sub2.py
+++ sub2.py
@@ -1,16 +1,16 @@
 #カプセル化
 
-class Reactangle:
+class Data:
     def __init__(self):
         self.nums = [1,2,3,4,5]
 
-        def change_data(self,index,n):
-            self.nums[index] = n
+    def change_data(self,index,n):
+        self.nums[index] = n
 
 data_one = Data()
 data_one.nums[0] = 100
 print(data_one.nums)
 
 data_two = Data()
-data_two.nums[0] = (0,100)
+data_two.change_data(0,100)
 print(data_two.nums)
